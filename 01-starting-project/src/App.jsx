d299bcb604c20243f1dcab6170e14b8b76a72dd1
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-07-15 20:04:04 -0700

    Storing Components in Files & Using a Good Project Structure

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,38 +1,7 @@
-import reactImg from './assets/react-core-concepts.png';
-// import componentsImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-
-
-function Header() {
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.img} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-
-  );
-}
 
 function App() {
   return (
