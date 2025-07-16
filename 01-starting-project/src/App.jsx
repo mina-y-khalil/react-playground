3794cb785d4b40d7150fd40aa3a39d5323a6ff85
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-07-16 16:49:54 -0700

    Deriving & Outputting Data Based on State

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 
 function App() {
-  const [tabContent, setTabContent] = useState("Please select a button"); // this must be on the top level of the component not inside any function the variable here is an array of 2 elements the first is the current value of the state the second is a function to update that value
+  const [tabContent, setTabContent] = useState("components"); // this must be on the top level of the component not inside any function the variable here is an array of 2 elements the first is the current value of the state the second is a function to update that value
 
   function handleSelect(selectedButton) {
     // selectedButton is the text content of the button that was clicked 'components', 'JSX', 'Props', 'State'
@@ -36,7 +36,16 @@ function App() {
             <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
           </menu>
-          {tabContent}
+          <div id="tab-content">
+            <h3>{EXAMPLES[tabContent.toLocaleLowerCase()].title}</h3>
+            <p>{EXAMPLES[tabContent.toLocaleLowerCase()].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[tabContent.toLocaleLowerCase()].code}
+              </code>
+            </pre>
+          </div>
+          {/* {tabContent} */}
         </section>
 
 
