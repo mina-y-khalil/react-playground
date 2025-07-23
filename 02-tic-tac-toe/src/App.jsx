1d5b946cf9703c474efc295f9831a9fba52c725f
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-07-22 19:39:12 -0700

    Rendering Multi-Dimensional Lists

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,4 +1,5 @@
 import Player from './components/Player.jsx';
+import Gameboard from './components/Gameboard.jsx';
 function App() {
 
 
@@ -10,7 +11,7 @@ function App() {
           <Player initialName="Player 2" symbol="O" />
         </ol>
 
-        GAME BOARD
+        <Gameboard />
 
       </div>
 
