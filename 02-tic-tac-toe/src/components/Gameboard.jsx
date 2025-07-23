86ca0eb9f5c70d76b1d33823eff1397901366ae1
mina-y-khalil <mina.y.khalil90@gmail.com>
2025-07-22 20:39:32 -0700

    Updating Object State Immutably

--- 02-tic-tac-toe/src/components/Gameboard.jsx
+++ 02-tic-tac-toe/src/components/Gameboard.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+
+//2D array
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,16 +8,28 @@ const initialGameBoard = [
 ];
 
 export default function Gameboard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])]; // here is brand new array from prev state and here we are updating it immutably and this is strongly recommended
+            updatedGameBoard[rowIndex][colIndex] = 'X';
+            return updatedGameBoard;
+        });
+
+    }
 
 
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
+            {gameBoard.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
-                            <li key={colIndex}><button>{playerSymbol}</button></li>
+                            <li key={colIndex}>
+                                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                            </li>
                         ))}
                     </ol>
                 </li>
